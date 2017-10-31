b7fa2d00ce5dfaea6d20446776105982db621887
CAViTECgames <cavitecgames@gmail.com>
2017-10-31 14:06:54 +0100

    Commit after adding some HUD elements

---
+++ Assets/Scripts/Controllers/ConfigController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigController : MonoBehaviour {
+
+	public int characterActualLife=0;
+	public int characterMaxLife = 100;
+	public int carriageMaxLife = 100;
+	public int banditAgressiveMaxLife = 100;
+	public int wolfMaxLife = 100;
+	public int banditNegotiatorMaxLife = 100;	
+	public int characterSpeed = 5;
+	public int carriageSpeed = 3;
+	public int wolfSpeed = 4;
+	public int banditAgressiveSpeed= 4;
+	public int banditNegotiatorSpeed = 4;
+
+	// Use this for initialization
+	void Start () {
+		characterActualLife = characterMaxLife;
+		characterActualLife = carriageMaxLife;
+	}
+
+	public int GetLife(string objective){
+
+		switch (objective) {
+
+		case "character": 
+			return characterMaxLife;
+		case "carriage":
+			return carriageMaxLife;
+		case "wolf":
+			return wolfMaxLife;
+		case "banditAgressive":
+			return banditAgressiveMaxLife;
+		case "banditNegotiator":
+			return banditNegotiatorMaxLife;
+		default:
+			return 0;
+		}
+	}
+
+	public int GetSpeed(string objective){
+
+		switch (objective) {
+
+		case "character": 
+			return characterSpeed;
+		case "carriage":
+			return carriageSpeed;
+		case "wolf":
+			return wolfSpeed;
+		case "banditAgresive":
+			return banditAgressiveSpeed;
+		case "banditNegotiator":
+			return banditNegotiatorSpeed;
+		default:
+			return 0;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Controllers/InterfaceController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceController : MonoBehaviour {
+	public Scrollbar healthBar;
+	public Image mask;
+	public Image life;
+	public Image frame;
+	public int lifeValue;
+	public Text[] carriagesText = new Text[5];
+	public Text clues;
+
+	void Start()
+	{		
+		State_Bar(false);
+		for(int i=0 ; i<=5 ; i++){
+			carriagesText[i].text = "" + GameObject.Find ("ConfigController").GetComponent<ConfigController>().carriageMaxLife;
+		}
+		clues.text = 0.ToString();
+	}
+
+	public void State_Bar(bool state){
+
+		healthBar.enabled = state;
+		mask.enabled = state;
+		life.enabled = state;
+		frame.enabled = state;
+	}
+	public void resetLife(){
+
+		lifeValue = GameObject.Find ("ConfigController").GetComponent<ConfigController>().characterMaxLife;
+	}
+
+	public void dealDamageCharacter(int damage)
+	{ //Resta a la vida la entrada damage
+		lifeValue = lifeValue - damage;
+		if (lifeValue <= 0)
+		{
+			lifeValue = 0;
+			Destroy(this.gameObject);
+		}
+		healthBar.size = lifeValue / 100f;
+	}
+	public void dealDamageCarriage(int objective, int damage){
+		int actual = 0;
+
+		Int32.TryParse(carriagesText[objective].text, out actual);
+		if(actual>damage){
+			carriagesText[objective].text= (actual-damage).ToString();
+		}else{
+			carriagesText[objective].text= "X";
+		}
+	}
+		
+}
