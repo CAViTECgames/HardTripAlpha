2c6e4c6073e459fbd387aeb4a9a9a296eb823603
Defu <gudo35@gmail.com>
2017-11-07 18:41:35 +0100

    Añadido drop controller

---
+++ Assets/Scripts/Controllers/DropController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DropController : MonoBehaviour
+{
+
+    // int[] drops = new int[3];
+    [System.Serializable]
+    public class DropCurrency {
+
+        public GameObject items;
+        public int dropRarity;
+}
+
+    public List<DropCurrency> LootTable = new List<DropCurrency>();
+    public int dropChance;
+    void calculateLoot()
+    {
+        int calc_dropChance = Random.Range(0, 101);
+
+        if (calc_dropChance > dropChance)
+        {
+            return;
+        }
+        if (calc_dropChance <= dropChance)
+        {
+            int itemWeight = 0;
+
+            for (int i = 0; i < LootTable.Count; i++)
+            {
+                itemWeight += LootTable[i].dropRarity;
+            }
+            Debug.Log("Itemweight=" + itemWeight);
+
+            int randomValue = Random.Range(0, itemWeight);
+
+            for (int j = 0; j < LootTable.Count; j++)
+            {
+                if (randomValue <= LootTable[j].dropRarity)
+                {
+                    Instantiate(LootTable[j].items, transform.position, Quaternion.identity);
+                }
+                    
+            
+            }
+                    
+         }
+                    
+
+      
+     }
+}
+
+
+   
+    
+ 
